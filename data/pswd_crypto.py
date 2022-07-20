a7e0af40ba61d3b1dfbde8259578d6304a29c073
Luis Ruiz Caracuel <86871709+LoXewyX@users.noreply.github.com>
2022-07-20 09:40:32 +0200

    Error text with mistaken grammar

--- data/pswd_crypto.py
+++ data/pswd_crypto.py
@@ -7,7 +7,7 @@ def encode(pswd):
     try:
         return fernet.encrypt(pswd.encode()).decode()
     except:
-        print("Error on decoding")
+        print("Error on encoding")
 def decode(pswd):
     try:
         return fernet.decrypt(pswd.encode()).decode()
